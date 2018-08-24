b0d3e9f48ac022ca967c517b3da8fccfafdf1442
sushichucha <sushmachucha@gmail.com>
2018-08-24 16:22:03 +0530

    Main program

---
+++ booleanAnd.java
@@ -0,0 +1,12 @@
+import java.io.*;
+class booleanOR
+{
+ public static void main(String args[])
+{
+ boolean firstnumber,secondnumber,result;
+firstnumber=false;
+secondnumber=false;
+result=firstnumber^secondnumber;
+System.out.println("result pathagonal is"+result);
+}
+}
